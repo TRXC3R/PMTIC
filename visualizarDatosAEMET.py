12acc280de83760e6a908b7dc850397d1068a701
TRXC3R <127333448+TRXC3R@users.noreply.github.com>
2025-01-08 15:01:21 +0100

    Update visualizarDatosAEMET.py

--- visualizarDatosAEMET.py
+++ visualizarDatosAEMET.py
@@ -12,11 +12,17 @@ def main():
     try:
         # Inicialización del componente principal
         componente = EstacionesAEMET.EstacionesAEMET(name='Panel de estaciones meteorológicas')
+        #text_input_provinicas = pn.widgets.TextInput(name = "Provincias")
+
+        #Sincronizar los widgets con el parametro
+        #pn.bind(lambda v, setattr(componente, 'provinicas', v), text_input_provincias)
         
         # Diseño interactivo con título y elementos organizados en columnas
         layout = pn.Column(
             "## Visualización de Datos Meteorológicos",
             pn.Row(componente.param, componente.plot)
+            #pn.Row(componente.param, componente.plot, text_input_provinicas)
+
         )
         
         # Mostrar la interfaz (puede usarse servable() para servidores web)
